6e6c9fbad490edbdaefc4831d04165bd97b35aaf
OyakiMorse <legenda1200@gmail.com>
2021-04-14 19:00:35 +0300

    add adaptive design

--- app/js/main.js
+++ app/js/main.js
@@ -4,22 +4,80 @@ $(function() {
         arrows: false,
         slidesToShow: 10,
         slidesToScroll: 10,
-        autoplay: true
+        autoplay: true,
+        responsive: [
+            {
+              breakpoint: 1700,
+              settings: {
+                slidesToShow: 8,
+                slidesToScroll: 8,
+              }
+            },
+            {
+              breakpoint: 1510,
+              settings: {
+                slidesToShow: 6,
+                slidesToScroll: 6,
+              }
+            },
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+              }
+            },
+            {
+              breakpoint: 840,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            },
+            {
+              breakpoint: 550,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                dots: false,
+              }
+            },
+            {
+              breakpoint: 376,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+              }
+            },
+            {
+              breakpoint: 320,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+              }
+            },
+            
+          ]
     });
+
+    $('.header__btn').on('click', function() {
+        $('.rightside-menu').removeClass('rightside-menu--close');
+    });
+    $('.rightside-menu--close').on('click', function() {
+        $('.rightside-menu').addClass('rightside-menu--close');
+    })
+
+    $('.header__btn-menu').on('click', function() {
+        $('.menu').toggleClass('menu--open')
+    })
+
+    if($(window).width() <= 650){
+        $('.works-path__item--measure').appendTo($('.works-path__items-box'))
+    }
+    
 });
-
-
-const headerBtn = document.getElementById('headerBtn')
-const rightsideMenu = document.getElementById('rightsideMenu')
-const rightMenuClose = document.getElementById('rightMenuClose')
-
-headerBtn.addEventListener('click', () => {
-    rightsideMenu.classList.remove('rightside-menu--close')
-})
-
-rightMenuClose.addEventListener('click', () => {
-    rightsideMenu.classList.add('rightside-menu--close')
-})
 
 const mixer = mixitup('.gallery__inner', {
     load: {
